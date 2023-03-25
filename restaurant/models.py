2b64ae8305c8bc5769088c83b776647dc978b715
Mahmoud Haney <mahmoudhaney25@gmail.com>
2023-03-25 14:03:23 +0200

    Database connection and Models are set

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(null=True, blank=True)
+    booking_date = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    inventory = models.IntegerField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# def sayHello(request):
+    # return HttpResponse("Hello to our Little Lemon Restaurant")
+
+def index(request):
+    return render(request, 'index.html', {})
